68fc0f6eb64912b90940bf4e82b5392b0c8b3a46
d02d33pak <deepak030190@gmail.com>
2020-03-02 14:26:18 +0530

    async/await working

--- downloader.py
+++ downloader.py
@@ -1,6 +1,9 @@
 import os
-import curio
+import asyncio
+import aiohttp
+import aiofiles
 import requests
+from timeit import default_timer
 from bs4 import BeautifulSoup
 
 
@@ -23,14 +26,17 @@ def download_category(soup, path, category):
     print(f">>Downloading '{category}' category")
 
     for i,event in enumerate(event_links):
+        start_time = default_timer()
         makeDir(event_names[i][:4])     # year-wise folder
         makeDir(event_names[i][5:])     # event per year)
-        print(f'[{i+1}] {event_names[i][:4]} → {event_names[i][5:]}')
+        print(f'[{i+1}/{len(event_links)}] {event_names[i][:4]} → {event_names[i][5:]}')
         content = requests.get(event).text
         image_soup = BeautifulSoup(content, 'lxml')
-        curio.run(download_images, image_soup)
+        asyncio.run(download_images(image_soup))
         os.chdir('..')      # come out of event dir
         os.chdir('..')      # come out of year dir
+        elapsed_time = default_timer() - start_time 
+        print(f'\tTime Elapsed: {elapsed_time:0.3f}\n')
 
 
 def makeDir(folder_name):
@@ -48,32 +54,31 @@ async def download_images(event_soup):
     counter = skips = success = 0
     for pic in photos: 
         title = pic.div.a.get('title', '') 
-        # many photos on motogp have the same file name so
-        # formatting to 00N format 
-        title = str(counter).zfill(2) + ' ' + title + '.jpg'
+        title = str(counter+1).zfill(2) + ' ' + title + '.jpg'
         dl_link = pic.div.a.get('data-image-fullscreen', '')
         try:
-            image_file = await curio.run_in_thread(requests.get, dl_link)
-            image_file = image_file.content
-            if (not os.path.exists(title)) :
-                with open(title, 'wb') as output:
-                    output.write(image_file)
-                    success+=1
-                    print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
-            # ! TODO: better skipping method below
-            elif os.path.getsize(title) != len(image_file):
-                with open(title, 'wb') as output:
-                    output.write(image_file)
-                    success+=1
-                    print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
-            else:
-                # * if exact file already exists skip the download
-                skips+=1
-                print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
+            async with aiohttp.ClientSession() as session:
+                async with session.get(dl_link) as image_file:
+                    if (not os.path.exists(title)) :
+                        jpg_image = await aiofiles.open(title, 'wb')
+                        await jpg_image.write(await image_file.read())
+                        success+=1
+                        await jpg_image.close()
+                        print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
+                    elif os.path.getsize(title) != len(await image_file.read()):
+                        jpg_image = await aiofiles.open(title, 'wb')
+                        await jpg_image.write(await image_file.read())
+                        success+=1
+                        await jpg_image.close()
+                        print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
+                    else:
+                        # * if exact file already exists skip the download
+                        skips+=1
+                        print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\t', end='\r')
 
         except Exception as err:
             print(err)
         finally:
             counter+=1
 
-    print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}\n')
+    print(f'\tTOTAL:{len(photos):03} Dowloaded:{success:03} Skipped:{skips:03}')
